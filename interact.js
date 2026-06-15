2029fc6bd0098a0555dd0bb00386cdfcba8296e1
Nxce_116 <nicenorytoms@gmail.com>
2026-06-16 01:54:16 +0700

    animation

--- interact.js
+++ interact.js
@@ -7,19 +7,22 @@ const Close = document.getElementById("close")
 bar = "side"
 topbar.style.top = '-100px'
 topbar.style.opacity = '0'
+topbar.style.scale = '0.3'
 
 function checkbar() {
     if (bar === "side") {
         sidebar.style.left = '-600px',
         sidebar.style.opacity = '0'
-        topbar.style.top = '30px'
+        topbar.style.top = '20px'
         topbar.style.opacity = '1'
+        topbar.style.scale = '1'
         bar = "float"
     } else {
         sidebar.style.left = '0',
         sidebar.style.opacity = '1'
         topbar.style.top = '-200px'
         topbar.style.opacity = '0'
+        topbar.style.scale = '0.3'
         bar = "side"
     }
 }
