477746b746e4be7a6dd812a27c98bcc5179f2574
Nxce_116 <nicenorytoms@gmail.com>
2026-06-16 01:06:59 +0700

    Update anti swear and add more library

--- interact.js
+++ interact.js
@@ -1,22 +1,45 @@
 const sidebar = document.getElementById("side-bar")
 const topbar = document.getElementById("topbar")
+const Close = document.getElementById("close")
 
 // pre check value 
-bar = "float"
+
+bar = "side"
 topbar.style.top = '-100px'
-topbar.style.translate ='-200px 0'
+topbar.style.opacity = '0'
 
 function checkbar() {
     if (bar === "side") {
         sidebar.style.left = '-600px',
+        sidebar.style.opacity = '0'
         topbar.style.top = '30px'
+        topbar.style.opacity = '1'
         bar = "float"
     } else {
         sidebar.style.left = '0',
+        sidebar.style.opacity = '1'
         topbar.style.top = '-200px'
+        topbar.style.opacity = '0'
         bar = "side"
     }
 }
 
+function hide() {
+    if (hide === "no") {
+        sidebar.style.width = '100px'
+        hide = "yes"
+    } else {
+        sidebar.style.width = '400px'
+        hide = "no"
+    }
+}
+
 sidebar.addEventListener('click', checkbar)
-topbar.addEventListener('click', checkbar)
+Close.addEventListener('click', checkbar)
+search.addEventListener('input', ()=>{
+    const word = search.value;
+    console.log(word)
+    if (word === "kuay") {
+        alert("Kuay Rai I sus")
+    }
+});
