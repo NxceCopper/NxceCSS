b4608e947b179fcd7d1d5939f135dd9ad12f1c4c
Nxce_116 <nicenorytoms@gmail.com>
2026-06-16 23:43:08 +0700

    rewind

--- interact.js
+++ interact.js
@@ -1,43 +1,44 @@
-const sidebar = document.getElementById("side-bar")
-const topbar = document.getElementById("topbar")
+const SideBar = document.getElementById("side-bar")
+const Topbar = document.getElementById("topbar")
 const Close = document.getElementById("close")
+const Content = document.getElementById("content")
 
 // pre check value 
 
-bar = "side"
-topbar.style.top = '-100px'
-topbar.style.opacity = '0'
-topbar.style.scale = '0.3'
+SideBarState = true
+Topbar.style.top = '-100px'
+Topbar.style.opacity = '0'
+Topbar.style.scale = '0.3'
 
 function checkbar() {
-    if (bar === "side") {
-        sidebar.style.left = '-600px',
-        sidebar.style.opacity = '0'
-        topbar.style.top = '20px'
-        topbar.style.opacity = '1'
-        topbar.style.scale = '1'
-        bar = "float"
+    if (SideBarState === true) {
+        SideBar.style.left = '-600px',
+        SideBar.style.opacity = '0'
+        Topbar.style.top = '20px'
+        Topbar.style.opacity = '1'
+        Topbar.style.scale = '1'
+        SideBarState = false
     } else {
-        sidebar.style.left = '0',
-        sidebar.style.opacity = '1'
-        topbar.style.top = '-200px'
-        topbar.style.opacity = '0'
-        topbar.style.scale = '0.3'
-        bar = "side"
+        SideBar.style.left = '0',
+        SideBar.style.opacity = '1'
+        Topbar.style.top = '-200px'
+        Topbar.style.opacity = '0'
+        Topbar.style.scale = '0.3'
+        SideBarState = true
     }
 }
 
 function hide() {
     if (hide === "no") {
-        sidebar.style.width = '100px'
+        SideBar.style.width = '100px'
         hide = "yes"
     } else {
-        sidebar.style.width = '400px'
+        SideBar.style.width = '400px'
         hide = "no"
     }
 }
 
-sidebar.addEventListener('click', checkbar)
+SideBar.addEventListener('click', checkbar)
 Close.addEventListener('click', checkbar)
 search.addEventListener('input', ()=>{
     const word = search.value;
@@ -46,4 +47,3 @@ search.addEventListener('input', ()=>{
         alert("Kuay Rai I sus")
     }
 });
-
